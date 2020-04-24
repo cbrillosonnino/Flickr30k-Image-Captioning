ff9ab956a49838734ac7661776c3adc862b16682
Charles <charles@pop-os.localdomain>
2020-04-24 11:01:03 -0700

    training routine

---
+++ train.py
@@ -0,0 +1,173 @@
+import argparse, os
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torch.nn.utils.rnn import pack_padded_sequence
+import torch.backends.cudnn as cudnn
+
+import pickle
+from utils import Vocabulary, Custom_Flickr30k, collate_fn
+from models import EncoderCNN, DecoderRNNwithAttention
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Flickr30k Training')
+    parser.add_argument('-batch_size', type=int, default=512,
+                        help='batch size')
+    parser.add_argument('-embed_size', type=int, default=256,
+                        help='embedding demension size')
+    parser.add_argument('-hid_size', type=int, default=512,
+                        help='hidden demension size')
+    parser.add_argument('-attn_size', type=int, default=512,
+                        help='attention demension size')
+    parser.add_argument('-drop', type=float, default=0.5,
+                        help='dropout percentage')
+    parser.add_argument('-epoch', type=int, default=300,
+                        help='number of training epochs')
+    parser.add_argument('-fine_tune', type=bool, default=True,
+                        help='whether to fine-tune the encoder or not')
+    parser.add_argument('-lr', type=float, default=0.01,
+                        help='initial learning rate')
+    parser.add_argument('--save', type=str, default=Path.cwd(),
+                        help='directory to save logs and models.')
+    parser.add_argument('--resume', default='', type=str,
+                        help='path to latest checkpoint (default: none)')
+    return parser
+
+
+def main():
+
+    cudnn.benchmark = True
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    args = get_parser().parse_args()
+
+    NUM_WORKERS = 2
+    CROP_SIZE = 256
+    NUM_PIXELS = 64
+    ENCODER_SIZE = 2048
+    learning_rate = args.lr
+    start_epoch = 0
+
+    min_ppl = 1_000_000
+    min_BLEU = 1_000_000
+
+    vocab = pickle.load(open('vocab.p', 'rb'))
+
+    train_transform = transforms.Compose([
+            transforms.RandomCrop(CROP_SIZE),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.444, 0.421, 0.385),
+                                 (0.285, 0.277, 0.286))])
+
+    val_transform = transforms.Compose([
+            transforms.CenterCrop(CROP_SIZE),
+            transforms.ToTensor(),
+            transforms.Normalize((0.444, 0.421, 0.385),
+                                 (0.285, 0.277, 0.286))])
+
+    train_loader = torch.utils.data.DataLoader(
+            dataset=Custom_Flickr30k('../flickr30k-images','../flickr30k-captions/results_20130124.token', vocab, transform=train_transform, train=True),
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=NUM_WORKERS,
+            collate_fn=collate_fn)
+
+    val_loader = torch.utils.data.DataLoader(
+            dataset=Custom_Flickr30k('../flickr30k-images','../flickr30k-captions/results_20130124.token', vocab, transform=val_transform, train=False),
+            batch_size=args.batch_size,
+            shuffle=False,
+            num_workers=NUM_WORKERS,
+            collate_fn=collate_fn)
+
+    # Initialize models
+    encoder = EncoderCNN(args.fine_tune).to(device)
+    decoder = DecoderRNNwithAttention(len(vocab), args.embed_size, args.hid_size, 1, args.attn_size, ENCODER_SIZE, NUM_PIXELS, dropout=args.drop).to(device)
+
+    # Initialize optimization
+    criterion = torch.nn.CrossEntropyLoss()
+    if args.fine_tune:
+        params = list(encoder.parameters()) + list(decoder.parameters())
+    else:
+        params = list(decoder.parameters()) + list(encoder.linear.parameters()) + list(encoder.bn.parameters())
+    optimizer = torch.optim.Adam(params, lr=learning_rate)
+
+    if args.resume:
+        if os.path.isfile(args.resume):
+            checkpoint = torch.load(args.resume)
+            start_epoch = checkpoint['epoch']
+            min_BLEU = checkpoint['min_BLEU']
+            encoder.load_state_dict(checkpoint['encoder'])
+            decoder.load_state_dict(checkpoint['decoder'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+        else: print("No checkpoint found at '{}'".format(args.resume))
+
+    for epoch in range(start_epoch, args.epoch):
+        for i, (images, captions, lengths) in enumerate(train_loader):
+
+            print(i)
+
+            # Batch to device
+            images = images.to(device)
+            captions = captions.to(device)
+            targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
+
+            encoder.train()
+            decoder.train()
+
+            features = encoder(images)
+            predictions, attention_weights = decoder(features, captions, lengths)
+
+            scores = pack_padded_sequence(predictions[:,:-1,:], torch.tensor(lengths)-2, batch_first=True).cpu()
+            targets = pack_padded_sequence(captions[:,1:-1], torch.tensor(lengths)-2, batch_first=True).cpu()
+
+            loss = criterion(scores.data, targets.data)
+
+            decoder.zero_grad()
+            encoder.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        if epoch % 50 == 0:
+            learning_rate /= 5
+            for param_group in optimizer.param_groups: param_group['lr'] = learning_rate
+
+        ## TODO: Validation
+
+        is_best = BLEU < min_BLEU
+        min_BLEU = max(BLEU, min_BLEU)
+        save_checkpoint({
+            'epoch': epoch + 1, 'encoder': encoder.state_dict(), 'decoder': decoder.state_dict(),
+            'min_BLEU': min_BLEU, 'optimizer' : optimizer.state_dict(),
+        }, is_best)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, f'{args.save_dir}/model_best.pth.tar')
+
+
+if __name__ == '__main__': main()
---
+++ utils.py
@@ -0,0 +1,175 @@
+from torchvision.datasets import VisionDataset
+import os
+from collections import defaultdict
+from PIL import Image
+import numpy as np
+import nltk
+from collections import Counter
+import pickle
+
+import torch
+
+class Vocabulary(object):
+    """Simple vocabulary wrapper."""
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
+
+    def add_word(self, word):
+        if not word in self.word2idx:
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
+
+    def __call__(self, word):
+        if not word in self.word2idx:
+            return self.word2idx['<unk>']
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.word2idx)
+
+    def get_id(self, w):
+        return self.word2idx[w]
+
+    def encode_seq(self, l):
+        return [self.word2idx[i] if i in self.word2idx else self.word2idx['<unk>'] for i in l]
+
+    def get_token(self, idx):
+        return self.idx2word[idx]
+
+    def decode_seq(self, l):
+        return [self.idx2word[i] for i in l]
+
+
+# Adapted from https://pytorch.org/docs/stable/_modules/torchvision/datasets/flickr.html#Flickr30k
+class Custom_Flickr30k(VisionDataset):
+    """`Flickr30k Entities <http://web.engr.illinois.edu/~bplumme2/Flickr30kEntities/>`_ Dataset.
+
+    Args:
+        root (string): Root directory where images are downloaded to.
+        ann_file (string): Path to annotation file.
+        vocabulary (object): Vocabulary wrapper
+        transform (callable, optional): A function/transform that takes in a PIL image
+            and returns a transformed version. E.g, ``transforms.ToTensor``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        is_train (boolean): Train set or val set
+    """
+
+    def __init__(self, root, ann_file, vocabulary, train=True, transform=None, target_transform=None):
+        super(Custom_Flickr30k, self).__init__(root, transform=transform, target_transform=target_transform)
+        self.ann_file = os.path.expanduser(ann_file)
+        self.train = train
+        self.annotations = defaultdict(list)
+        self.punc_set = set([',',';',':','.','?','!','(',')'])
+        self.vocabulary = vocabulary
+
+        if self.train:
+            split = pickle.load(open('train_set.p', 'rb'))
+            idx=0
+            with open(self.ann_file) as fh:
+                for line in fh:
+                    img_id, caption = line.strip().split('\t')
+                    img = img_id[:-2]
+                    if img in split:
+                        self.annotations[idx].extend([img,caption])
+                        idx += 1
+            self.ids = np.arange(idx-1)
+
+        else:
+            split = pickle.load(open('val_set.p', 'rb'))
+            idx = -1
+            last_image = ''
+            with open(self.ann_file) as fh:
+                for line in fh:
+                    img_id, caption = line.strip().split('\t')
+                    img = img_id[:-2]
+                    if img in split:
+                        if img != last_image:
+                            idx += 1
+                            last_image = img
+                            self.annotations[idx].append(img)
+                            self.annotations[idx].append([caption])
+                        else:
+                            self.annotations[idx][1].append(caption)
+            self.ids = np.arange(idx)
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: Tuple (image, target). target is a list of captions for the image.
+        """
+        instance = self.annotations[index]
+
+        img_id = instance[0]
+        caption = instance[1]
+
+        # Image
+        filename = os.path.join(self.root, img_id)
+        img = Image.open(filename).convert('RGB')
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.train:
+
+            # Captions
+            tokens = nltk.tokenize.word_tokenize(str(caption).lower())
+            caption = []
+            caption.append(self.vocabulary('<start>'))
+            caption.extend([self.vocabulary(token) for token in tokens if token not in self.punc_set])
+            caption.append(self.vocabulary('<end>'))
+            target = torch.Tensor(caption)
+
+        else:
+            # Captions
+            target = []
+            for item in caption:
+                tokens = nltk.tokenize.word_tokenize(str(item).lower())
+                caption = []
+                caption.append(self.vocabulary('<start>'))
+                caption.extend([self.vocabulary(token) for token in tokens if token not in self.punc_set])
+                caption.append(self.vocabulary('<end>'))
+                target.append(torch.Tensor(caption))
+
+        return img, target
+
+
+    def __len__(self):
+        return len(self.ids)
+
+
+def collate_fn(data):
+    """Creates mini-batch tensors from the list of tuples (image, caption).
+
+    We should build custom collate_fn rather than using default collate_fn,
+    because merging caption (including padding) is not supported in default.
+
+    Args:
+        data: list of tuple (image, caption).
+            - image: torch tensor of shape (3, 256, 256).
+            - caption: torch tensor of shape (?); variable length.
+
+    Returns:
+        images: torch tensor of shape (batch_size, 3, 256, 256).
+        targets: torch tensor of shape (batch_size, padded_length).
+        lengths: list; valid length for each padded caption.
+    """
+    # Sort a data list by caption length (descending order).
+    data.sort(key=lambda x: len(x[1]), reverse=True)
+    images, captions = zip(*data)
+
+    # Merge images (from tuple of 3D tensor to 4D tensor).
+    images = torch.stack(images, 0)
+
+    # Merge captions (from tuple of 1D tensor to 2D tensor).
+    lengths = [len(cap) for cap in captions]
+    targets = torch.zeros(len(captions), max(lengths)).long()
+    for i, cap in enumerate(captions):
+        end = lengths[i]
+        targets[i, :end] = cap[:end]
+    return images, targets, lengths
