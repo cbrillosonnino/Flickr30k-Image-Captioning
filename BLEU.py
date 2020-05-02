c0ebc27c9835e199665b5678cb465a1fa4e6ee6e
Darren <dg2458@nyu.edu>
2020-05-02 14:09:58 -0400

    add beam search support to BLEU

--- BLEU.py
+++ BLEU.py
@@ -3,12 +3,11 @@ import nltk
 import numpy as np
 import torch
 
-def bleu_eval(encoder, decoder, data_loader, batch_size, device):
+def bleu_eval(encoder, decoder, data_loader, batch_size, device, beam_size=None):
     with torch.no_grad():
         true_outputs = [] # e.g: [[ref1_1, ref1_2], [ref2_1, ...], ....]
         decoder_outputs = [] # e.g: [out1, out2, out3]
         for i, (images, captions, lengths) in enumerate(data_loader):
-            
             for caption in captions:
                 caption = caption.numpy().astype(str).tolist()
                 idx = 0
@@ -26,8 +25,23 @@ def bleu_eval(encoder, decoder, data_loader, batch_size, device):
 
             features = encoder(images)
             max_sequence_length = int(np.array(captions != 0).sum(axis = 1).mean()+1)
-            sample = decoder.sample(features, max_seq_length=max_sequence_length).cpu()
-            decoder_outputs.extend(sample.numpy().astype(str).tolist())
+            if beam_size is None: # greedy
+            	sample = decoder.sample(features, max_seq_length=max_sequence_length).cpu()
+            	decoder_outputs.extend(sample.numpy().astype(str).tolist())
+            else:
+                beams = decoder.beam_sample(features, 
+                    imgs=images, 
+                    targets=captions,
+                    beam_size=beam_size,
+                    max_seq_length=max_sequence_length)
+                for i in range(batch_size):
+                    best_score_idx = -1
+                    best_score = 0
+                    for j in range(len(beams)):
+                        if beams[j].scores[i] > best_score:
+                            best_score_idx = j
+                            best_score = beams[j].scores[i]
+                    decoder_outputs.append(beams[best_score_idx].seq[i])
 
         predictions = []
         for pred in decoder_outputs:
